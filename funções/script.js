6b6afeee812a6508be39907f552b867617001a0e
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-06 21:45:28 -0300

    funcao2

--- funções/script.js
+++ funções/script.js
@@ -88,4 +88,97 @@ function faltaVisitar(piasesVisitados) {
 console.log(totalPaises);
 
 
+var profissao = "Developer"
 
+function dados() {
+    var nome = "Hard";
+    var idade = 25;
+    function outrosDados() {
+        var endereco = 'Aquiraz-CE';
+        var idade = 23;
+        return  `${nome}, ${idade}, ${endereco}, ${profissao}`;
+    }
+    return outrosDados();
+}
+console.log(dados()); // Retorna 'Hard, 23, Aquiraz-CE, Developer'
+// outrosDados(); // retorna um erro
+
+
+// EXERCÍCIO
+
+// Crie uma função para verificar se um valor é Truthy
+
+function isTruthy(dado) {
+    return !!dado;
+}
+console.log(isTruthy('23'))
+
+// function bool(dado) {
+//     if (dado !== true) {
+//         return `Type False`;
+//     } else {
+//         return `Type True`;
+//     }
+// }
+// console.log(bool(true))
+
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetroQuadrado(lado) {
+    return lado * 4;
+}
+console.log(perimetroQuadrado(3));
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`;
+}
+
+console.log(nomeCompleto('Hard','GS'))
+
+// Crie uma função que verifica se um número é par
+
+function isEven(num) {
+    var modul = num % 2;
+    if (modul === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isEven(20))
+
+// Crie uma função que retorne o tipo de dado
+// do argumento passado nea (typeof)
+
+function tipoDeDado(dado) {
+    return typeof dado;
+}
+
+console.log(tipoDeDado(3))
+
+
+// addEventListener é uma função nativa d Javascript
+// o primeiro parâmetro é o evento que ocorre e o segundo Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'click' ocorrer.
+
+addEventListener('click', function() {
+    console.log('Hard')
+})
+
+// Corrija o erro abaixo
+// function precisoVisitar(piasesVisitados) {
+//     var totalPaises = 193;
+//     return `Ainda faltam ${totalPaises - piasesVisitados} paise para visitar`;
+// }
+// function jaVisitei(piasesVisitados) {
+//     return `Já visitei ${piasesVisitados} do total de ${totalPaises} paises`;
+// }
+// precisoVisitar(29);
+// jaVisitei(20);
