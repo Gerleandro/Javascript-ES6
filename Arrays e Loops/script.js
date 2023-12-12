f7e69e9250ac10f6690d294a469cd4c1f9f41864
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-11 21:47:32 -0300

    for e while

---
+++ Arrays e Loops/script.js
@@ -0,0 +1,13 @@
+var videoGames = ['Switch', 'PS4', 'XBOX'];
+
+var ultimoItem = videoGames.pop();
+
+for (var numero = 0; numero <= 10; numero++) {
+    console.log(numero);
+}
+
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
