3fd1adc085d91543643a4d21ca757e2e349f5881
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-13 18:19:58 -0300

    frutas

--- Arrays e Loops/script.js
+++ Arrays e Loops/script.js
@@ -15,4 +15,13 @@ var videoGames = ['Switch', 'PS4', 'XBOX', '3DS'];
 
 for(var item = 0; item < videoGames.length; item++) {
     console.log(videoGames[item]);
+    if (videoGames[item] === 'PS4') {
+        break;
+    }
 }
+
+var frutas = ['Banana','Pera','Maçã','Abacaxi','Uva'];
+
+frutas.forEach(function(item) {
+    console.log(item);
+})
