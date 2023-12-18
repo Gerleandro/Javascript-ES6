645a1b5e7d484d63350d94b6fa932d5ea76bd492
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-18 18:36:01 -0300

    titulo

---
+++ O que é o DOM/script.js
@@ -0,0 +1,7 @@
+const href = window.location.href;
+console.log(href);
+
+if (href === 'http://127.0.0.1:5500/O%20que%20%C3%A9%20o%20DOM/index.html') {
+    console.log(`href é igual a ${href}`);
+}
+
