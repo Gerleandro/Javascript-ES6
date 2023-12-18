51f9c6a1c38fcebbd0debb6f4e20dca7a9e08186
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-17 22:03:18 -0300

    Escopo

---
+++ Escopo/script.js
@@ -0,0 +1,75 @@
+// // var carro = 'Fusca';
+
+// // function mostrarCarro() {
+// //     console.log(carro);
+// // }
+
+// // mostrarCarro(); // Fusca no console
+// // console.log(carro); // Erro, carro is not defined
+
+
+// // if (false){
+// //     const mes = 'Dezembro';
+// //     console.log(mes);
+// // }
+// // // console.log(mes); 
+
+// // {
+// //     var carro2 = 'Kamaro';
+// //     const ano = 2018;
+// // }
+
+// // console.log(carro2); 
+// // // console.log(anos); 
+
+// // // var i = 50
+
+// // // for (let i = 0; i < 10; i++) {
+// // //     console.log(`Número ${i}`);
+// // // }
+
+// // console.log(i * 10);
+
+// const semana = 'Sexta';
+// // semana = 'Quinta';
+
+// console.log(semana);
+
+// const data = {
+//     ano: 2028,
+//     mes: 'Dezembro',
+// }
+// data.ano = 2019;
+// data.dia = 2025;
+
+// ============ EXERCICIO =============
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+    console.log(cor, marca, portas);
+}
+
+// como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+    return x + dois;
+}
+function dividirDois(x) {
+    return x / dois;
+}
+
+console.log(somarDois(8));
+console.log(dividirDois(50));
+
+// O que fazer para total retormar 500?
+const numero = 50;
+
+for (let numero = 0; numero <= 10; numero++) {
+    console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
