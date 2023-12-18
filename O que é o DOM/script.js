071132758ea1ff1876d3b11cf2583441b3bbb2ad
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2023-12-18 19:37:34 -0300

    callbackh1

--- O que é o DOM/script.js
+++ O que é o DOM/script.js
@@ -5,3 +5,11 @@ if (href === 'http://127.0.0.1:5500/O%20que%20%C3%A9%20o%20DOM/index.html') {
     console.log(`href é igual a ${href}`);
 }
 
+const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+function callbackh1() {
+    console.log('clicou em', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1);
