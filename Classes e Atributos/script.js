0fb06f0823e458158d0c70f71bd5409b17f58851
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2024-01-17 18:01:20 -0300

    atributos

--- Classes e Atributos/script.js
+++ Classes e Atributos/script.js
@@ -1,2 +1,18 @@
 const menu = document.querySelector('.menu');
-console.log(menu)
+
+menu.classList.add('ativo', 'teste');
+menu.classList.remove('azul');
+
+if(menu.classList.contains('azul')) {
+    menu.classList.add('possui-azul');
+} else {
+    menu.classList.add('nao-possui-azul');
+}
+
+menu.className = menu.className + ' testando'
+
+console.log(menu.className);
+
+// console.log(menu.classList.);
+
+const animais = document.querySelector('.animais');
