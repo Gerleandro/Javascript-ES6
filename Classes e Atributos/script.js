d88ef4d710f770a7a9e1971b307cbef22a03586d
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2024-01-21 16:40:44 -0300

    classes e atributos

--- Classes e Atributos/script.js
+++ Classes e Atributos/script.js
@@ -16,3 +16,58 @@ console.log(menu.className);
 // console.log(menu.classList.);
 
 const animais = document.querySelector('.animais');
+
+console.log(animals.attributes['data-texto']);
+
+const img = document.querySelector('img');
+
+console.log(img.getAttribute('alt'));
+
+const srcImg = img.getAttribute('alt');
+
+img.setAttribute('alt', 'é uma raposa');
+
+const possuiAlt = img.hasAttributes('title');
+
+console.log(possuiAlt);
+
+console.log(srcImg);
+
+const carro = {
+    portas: 4,
+    andar: function(km) {
+        console.log(`andou ${km}`);
+    }
+}
+
+//=========== EXERCICIO ============
+
+// Adicione a classe ativo a todos os itens do menu
+const itensMenu = document.querySelectorAll('.menu a');
+
+itensMenu.forEach((item) => {
+    item.classList.add('ativo');
+});
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas ano primeiro
+itensMenu.forEach((item) => {
+    item.classList.remove('ativo');
+});
+
+itensMenu[0].classList.add('ativo');
+
+
+// Verifique se as imagens possuem so atributo alt
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(() => {
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(img, possuiAtributo);
+});
+
+// Modifique o href do link externo no menu
+
+const link = document.querySelector('a[href^="http"]');
+link.setAttribute('href', 'hhtps://www.google.com/');
+
+console.log(link);
