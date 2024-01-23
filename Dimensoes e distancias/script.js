718c2ff52a60ff83cdc8101954be61563c980757
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2024-01-22 22:11:04 -0300

    dimensoes e distancias

---
+++ Dimensoes e distancias/script.js
@@ -0,0 +1,10 @@
+const animalsList = document.querySelector('.animals-list');
+
+const height = animalsList.scrollHeight;
+const animalsTop = animalsList.offsetTop;
+console.log(height);
+console.log(animalsTop);
+
+const primaryh2 = document.querySelector(' h2');
+const h2left = primaryh2.offsetLeft;
+console.log(primaryh2);
