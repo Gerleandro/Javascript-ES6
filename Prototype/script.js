76b6f57fb08df528c5ba68558118aa7bedff0cbc
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2024-04-02 13:15:55 -0300

    prototype

--- Prototype/script.js
+++ Prototype/script.js
@@ -1,50 +1,50 @@
-// // A propriedade prototype é um objeto adicionado a uma função quando a mesma é criada.
+// A propriedade prototype é um objeto adicionado a uma função quando a mesma é criada.
 
-// function Pessoa(nome, idade) {
-//     this.nome = nome;
-//     this.idade = idade;
-//     this.abracar = function() {
-//         return 'Abraçou';
-//     }
-// }
+function Pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.abracar = function() {
+        return 'Abraçou';
+    }
+}
 
-// var obj = {
-//     nome: 'Nadre',
-//     idade: 33,
-// }
+var obj = {
+    nome: 'Nadre',
+    idade: 33,
+}
 
-// Pessoa.prototype.andar = function() {
-//     return this.nome + ' Pessoa andou';
-// }
+Pessoa.prototype.andar = function() {
+    return this.nome + ' Pessoa andou';
+}
 
-// Pessoa.prototype.nadar = function() {
-//     return this.nome + ' Pessoa nadou';
-// }
+Pessoa.prototype.nadar = function() {
+    return this.nome + ' Pessoa nadou';
+}
 
-// const andre = new Pessoa('Andre', 28);
+const andre = new Pessoa('Andre', 28);
 
-// console.log(Pessoa.prototype);
-// console.log(andre.prototype);
+console.log(Pessoa.prototype);
+console.log(andre.prototype);
 
 
-// const pais = 'Brasil';
-// const cidade = new String('Rio');
+const pais = 'Brasil';
+const cidade = new String('Rio');
 
-// const listAnimals = ['Cachorro', 'Gato', 'Cavalo'];
+const listAnimals = ['Cachorro', 'Gato', 'Cavalo'];
 
-// const list = document.querySelectorAll('li');
+const list = document.querySelectorAll('li');
 
-// // Transforma em uma array
-// const listArray = Array.prototype.slice.call(list);
-// const listArray2 = Array.from(list);
+// Transforma em uma array
+const listArray = Array.prototype.slice.call(list);
+const listArray2 = Array.from(list);
 
-// const Carro = {
-//     marca: 'Ford',
-//     preco: 2000,
-//     andar() {
-//         return 'Oi';
-//     }
-// }
+const Carro = {
+    marca: 'Ford',
+    preco: 2000,
+    andar() {
+        return 'Oi';
+    }
+}
 
 
 
@@ -55,20 +55,36 @@
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
 
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`
+}
+
+const identidade = new Pessoa('joao', 'pedro', '25')
+
 // Lister os métodos acessados por
 // dados criados com NodeList,
-// HTMLCollection, Document
+// HTMLCollection, Document 
+
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
 
 // Liste os Construtores dos dados abaixo
 const li = document.querySelector("li");
 
-li;
-li.click;
-li.innerText;
-li.value;
-li.hidden;
-li.offsetLeft;
-li.click();
+li;// HTMLLIElement
+li.click;// Function
+li.innerText;// String
+li.value;// Number
+li.hidden;// Boolean
+li.offsetLeft;// Number
+li.click();// undefined
 
 // Qual o construtor do dado abaixo;
-li.hidden.constructor.name;
+li.hidden.constructor.name; // tipo de dado é string
