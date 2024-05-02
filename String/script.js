7a1bea767144c8ebd5de3da6c4dc9d6871f7d459
Gerleandro S Silva <98246658+Gerleandro@users.noreply.github.com>
2024-05-02 18:45:43 -0300

    Number and Math

--- String/script.js
+++ String/script.js
@@ -1,71 +1,71 @@
-// // const comida = "Pizza";
-// // const agua = new String("Agua ");
+// // // const comida = "Pizza";
+// // // const agua = new String("Agua ");
 
-// // console.log(agua.length);
+// // // console.log(agua.length);
 
-// // const frase = 'A melhor comida';
+// // // const frase = 'A melhor comida';
 
-// // console.log(frase[frase.length - 1]);
-// // console.log(frase.charAt(frase.length - 1));
+// // // console.log(frase[frase.length - 1]);
+// // // console.log(frase.charAt(frase.length - 1));
 
-// const frase = "A melhor linguagem é ";
-// const linguagem = "Javascript";
+// // const frase = "A melhor linguagem é ";
+// // const linguagem = "Javascript";
 
-// const fraseFinal = frase.concat(linguagem, " !!")
+// // const fraseFinal = frase.concat(linguagem, " !!")
 
-// const fruta = "Banana";
-// const listaFrutas = "Melancia, Banana, Laranja";
+// // const fruta = "Banana";
+// // const listaFrutas = "Melancia, Banana, Laranja";
 
-// console.log(listaFrutas.includes(fruta, 14));
+// // console.log(listaFrutas.includes(fruta, 14));
 
-// console.log(fruta.endsWith('nas'));
-// console.log(fruta)
+// // console.log(fruta.endsWith('nas'));
+// // console.log(fruta)
 
-// const transacao1 = 'Depósito de cliente';
-// const transacao2 = 'Depósito de fornecedor';
-// const transacao3 = 'Taxa de camisas';
+// // const transacao1 = 'Depósito de cliente';
+// // const transacao2 = 'Depósito de fornecedor';
+// // const transacao3 = 'Taxa de camisas';
 
-// console.log(transacao1.slice(0,3))
+// // console.log(transacao1.slice(0,3))
 
-// console.log(fruta.indexOf('a'))
-// console.log(fruta.lastIndexOf('a'))
+// // console.log(fruta.indexOf('a'))
+// // console.log(fruta.lastIndexOf('a'))
 
-// const preco = 'R$ 99,00';
+// // const preco = 'R$ 99,00';
 
-// const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
+// // const listaPrecos = ['R$ 99', 'R$ 199', 'R$ 12000'];
 
-// listaPrecos.forEach((item) => {
-//     console.log(item.padStart(10, '-'));
-// })
+// // listaPrecos.forEach((item) => {
+// //     console.log(item.padStart(10, '-'));
+// // })
 
-// const frase2 = "Ta";
+// // const frase2 = "Ta";
 
-// frase2.repeat(5); // TaTaTaTaTa
+// // frase2.repeat(5); // TaTaTaTaTa
 
-// let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
-// listaItens = listaItens.replace(/[ ]+/g, ', '); // add virgula entre palavras
-// listaItens = listaItens.replace("Camisas","Shirts")
+// // let listaItens = "Camisas Bonés Calças Bermudas Vestidos Saias";
+// // listaItens = listaItens.replace(/[ ]+/g, ', '); // add virgula entre palavras
+// // listaItens = listaItens.replace("Camisas","Shirts")
 
-// const arrayLista = listaItens.split("s, ")
-// console.log(arrayLista)
+// // const arrayLista = listaItens.split("s, ")
+// // console.log(arrayLista)
 
-// const htmlText = '<div>O melhor item</div><div>A melhor lista</div>';
-// const htmlArray = htmlText.split('div');
-// const novoHtml = htmlArray.join('section');
+// // const htmlText = '<div>O melhor item</div><div>A melhor lista</div>';
+// // const htmlArray = htmlText.split('div');
+// // const novoHtml = htmlArray.join('section');
 
-// console.log(htmlText);
-// console.log(htmlArray);
-// console.log(novoHtml)
-// const frutasArray = ['Banana', 'Melancia', 'Laranja'];
+// // console.log(htmlText);
+// // console.log(htmlArray);
+// // console.log(novoHtml)
+// // const frutasArray = ['Banana', 'Melancia', 'Laranja'];
 
-// const sexo1 = "Feminino";
-// const sexo2 = "feminino";
-// const sexo3 = "FEMININO";
+// // const sexo1 = "Feminino";
+// // const sexo2 = "feminino";
+// // const sexo3 = "FEMININO";
 
-// console.log(sexo1.toUpperCase() === "FEMININO");
+// // console.log(sexo1.toUpperCase() === "FEMININO");
 
 
-// ========== EXERCICIOS ==========
+// // ========== EXERCICIOS ==========
 
 // Utilizando o foreach na array abaixo,
 // some os valores de Taxa e os valores de Recebimento
