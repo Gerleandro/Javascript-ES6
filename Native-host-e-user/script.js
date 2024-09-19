b3f96906261a81049241f48ba705deb8a31cd281
unknown <gerlleandrosyllva@gmail.com>
2024-09-19 08:57:43 -0300

    array and interaction

--- Native-host-e-user/script.js
+++ Native-host-e-user/script.js
@@ -1,34 +1,6 @@
-// console.log(typeof Array.from !== "undefined")
-
-// if (typeof Array.from !== "undefined") {
-//     console.log("existe")
-// } else {
-//     console.log("Nao existe")
-// } 
-
-// ============== EXERCICIOS ===============
-
 // Liste 5 objetos nativos
-
-Object
-String
-Array
-Function
-Number
 
 // Liste 5 objetos do browser
 
-Window
-History
-Document
-HTMLCollection
-NodeList
-
-// liste 2 Métodos, Propriedades ou Objetos
+// Liste 2 métodos, propriedades ou Objetos
 // presentes no Chrome que não existem no Firefox
-
-if (typeof document.webkitVisibilityState !== "undefined") {
-    console.log('Existe');
-} else {
-    console.log('Não existe');
-}
